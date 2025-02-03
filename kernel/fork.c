425b39d940bf83f9841d926003fd938538daec9a
Winkmoon <142460047+Winkmoon@users.noreply.github.com>
2025-02-03 20:15:45 +0800

    更新 fork.c

--- kernel/fork.c
+++ kernel/fork.c
@@ -1841,10 +1841,9 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	syscall_tracepoint_update(p);
 	write_unlock_irq(&tasklist_lock);
 
-	cgroup_post_fork(p);
 	proc_fork_connector(p);
+	cgroup_post_fork(p);
 	sched_post_fork(p);
-	cgroup_post_fork(p, cgrp_ss_priv);
 	threadgroup_change_end(current);
 	perf_event_fork(p);
 
